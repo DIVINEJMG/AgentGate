04b9debefc3dee1bf06b62a84e6eb27592fe2106
Divine Ogadinma <148014402+DIVINEJMG@users.noreply.github.com>
2026-09-24 09:20:51 +0100

    F29.36-39 add __init__.py

---
+++ backend/app/environment/__init__.py
@@ -0,0 +1,11 @@
+from app.environment.model import (
+    EnvironmentSnapshot,
+    EnvironmentSystem,
+    ResourceRelationship,
+)
+
+__all__ = [
+    "EnvironmentSnapshot",
+    "EnvironmentSystem",
+    "ResourceRelationship",
+]
